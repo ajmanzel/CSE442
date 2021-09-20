31f259c9766fb106a29667d1afc326d8c9a15b78
wpdoughe <wpdoughe@buffalo.edu>
2021-09-19 23:54:57 -0400

    added ketysto gitignore

--- spotify/login.py
+++ spotify/login.py
@@ -4,7 +4,7 @@ from KEYS.spotifyKeys import *
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-scopes = "user-top-read streaming user-library-read playlist-read-collaborative user-read-currently-playing user-follow-read user-follow-modify user-read-email user-modify-playback-state user-read-recently-played app-remote-control user-read-playback-position user-read-playback-state ugc-image-upload"
+scopes = "user-top-read playlist-read-collaborative user-read-currently-playing user-follow-read user-follow-modify user-read-email user-modify-playback-state user-read-recently-played app-remote-control user-read-playback-position user-read-playback-state ugc-image-upload"
 
 test = "test"
 
@@ -13,4 +13,4 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
                                                redirect_uri=REDIRECT_URL,
                                                scope=scopes))
 res = sp.current_user_top_tracks(limit=1)
-print(os.system)
+print(res)
