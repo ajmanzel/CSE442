45eaf6c5b2178a62af55896bc81c4da1fafa2981
ajmanzel <47645440+ajmanzel@users.noreply.github.com>
2021-09-25 22:15:50 -0400

    getting bot back online, any imports need to be placed in the requirements.txt file so heroku knows where to get them for the deployment #15

--- discord/main.py
+++ discord/main.py
@@ -2,14 +2,15 @@ import discord
 from discord.ext import commands, tasks
 import youtube_dl
 import os
-import spotify.artist_info #I was required to turn spotify into a package with the use of the init
+import spotify.artist_info  # I was required to turn spotify into a package with the use of the init
+
 #                           file. Do not remove __init__.py from spotify or else this will no longer work.
-#from discKEYS.disctoken import *
+# from discKEYS.disctoken import *
 # Download the discKEYS file and put it in the ./CSE442/discord directory. Personal testing
 
 
 my_secret = os.environ.get('TOKEN')
-#my_secret = TOKEN
+# my_secret = TOKEN
 #
 
 client = commands.Bot(command_prefix='/')
@@ -24,6 +25,7 @@ async def on_ready():
 async def hello(cxt):
     await cxt.send("Hello World!")
 
+
 # This is how the bot calls topsongs. The API call made by Billy returns back the artistDict and the bot prints out "top songs"
 @client.command(pass_context=True)
 async def topsongs(cxt, name: str):
@@ -31,11 +33,13 @@ async def topsongs(cxt, name: str):
     topsongsList = artistDict["top songs"]
     spokenStr = "Here are " + name + "'s top songs:\n"
     for i in topsongsList:
-        if i == topsongsList[len(topsongsList)-1]:
+        if i == topsongsList[len(topsongsList) - 1]:
             spokenStr += i
         else:
             spokenStr += i + ",\n"
     await cxt.send(spokenStr)
+
+
 # Currently the issue this faces is within the API call itself. The name given to the bot can get confused and the API
 # returned may be a different artist that was close enough to the spelling. We gottta make sure to fix this.
 
