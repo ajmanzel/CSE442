782f7fb870c4d506dae7119cc7fb0dae1e2d68c9
Nordap64 <54553880+Nordap64@users.noreply.github.com>
2021-10-12 01:15:22 -0400

    #37 Play pause and resume all added and code cleaned and simplified.

--- bot/main.py
+++ bot/main.py
@@ -1,9 +1,9 @@
 import discord
 from discord.ext import commands, tasks
 from discord.player import FFmpegAudio
-import youtube_dl
 import os
 import artist_info
+import music
 from ytapi import get_youtube_data
 #from KEYS.disctoken import *    # Download the discKEYS file and put it in the ./CSE442/discord directory. Personal testing
 
@@ -11,6 +11,12 @@ my_secret = os.environ.get('TOKEN')
 #my_secret = TOKEN   # Comment this out before pushing please
 
 client = commands.Bot(command_prefix='/')
+intents = discord.Intents.all()
+cogs = [music]
+
+for i in range(len(cogs)):
+    cogs[i].setup(client)
+
 commandsList = ["hello: I wont leave you hanging", "ping: pOnG", "helpme: I assume you've already figured this out", "topsongs (Artist Name): I'll show you the top ten songs of whatever artist you choose"]
 
 
@@ -20,21 +26,22 @@ async def on_ready():
 
 
 @client.command(pass_context=True)
-async def hello(cxt):
-    await cxt.send("Hello World!")
+async def hello(ctx):
+    await ctx.send("Hello World!")
 
 
 @client.command(pass_context=True)
-async def helpme(cxt):
+async def helpme(ctx):
     currentCommands = "Hi I'm Discify, your all-purpose Discord Music Bot! \n Here's what I can do if you type /(command): \n"
     for i in commandsList:
         currentCommands += i + "\n"
-    await cxt.send(currentCommands)
+    await ctx.send(currentCommands)
 
 
 # This is how the bot calls topsongs. The API call made by Billy returns back the artistDict and the bot prints out "top songs"
 @client.command(pass_context=True)
-async def topsongs(cxt, name: str):
+async def topsongs(ctx, *namelst):
+    name = " ".join(namelst)
     artistDict = artist_info.getAll(name)
     topsongsList = artistDict["top songs"]
     spokenStr = "Here are " + name + "'s top songs:\n"
@@ -43,7 +50,7 @@ async def topsongs(cxt, name: str):
             spokenStr += i
         else:
             spokenStr += i + ",\n"
-    await cxt.send(spokenStr)
+    await ctx.send(spokenStr)
 
 
 # Currently the issue this faces is within the API call itself. The name given to the bot can get confused and the API
@@ -52,52 +59,17 @@ async def topsongs(cxt, name: str):
 
 # This is how the bot calls play. The API call made in ytapi.py returns the youtube_dict and the bot prints out the YouTube URL.
 @client.command(pass_context=True)
-async def play(cxt, query: str):
+async def url(ctx, *querylst):
+    query = " ".join(querylst)
     youtube_dict = get_youtube_data(query)
     youtube_url = youtube_dict['video_url']
     spoken_str = "YouTube URL: " + youtube_url
-    await cxt.send(spoken_str)
+    await ctx.send(spoken_str)
 
 
 @client.command(pass_context=True)
 async def ping(cxt):
     await cxt.send("Pong!")
-
-@client.command(pass_context=True)
-async def playsong(cxt, url: str):
-    #cxt.voice_client.stop()
-    if cxt.author.voice is None:
-        return await cxt.send("You're not in a voice channel!")
-    author = cxt.author
-    voice_channel = author.voice.channel
-    if cxt.voice_client is None:
-        await voice_channel.connect()
-    else:
-        await cxt.voice_client.disconnect()
-        await voice_channel.connect()
-
-    FFMPEG_OPTIONS = {
-        'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
-        'options': '-vn'
-    }
-    YDL_OPTIONS = {
-        'format': 'bestaudio'
-    }
-    vc = cxt.voice_client
-
-    with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
-        info = ydl.extract_info(url, download=False)
-        url2 = info['formats'][0]['url']
-        source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
-        vc.play(source)
-
-@client.command(pass_context=True)
-async def goodbye(cxt):
-    for x in client.voice_clients:
-        await cxt.send("Goodbye!")
-        return await x.disconnect()
-
-    return await cxt.send("I am not connected to any voice channel on this server!")
 
 
 client.run(my_secret)
