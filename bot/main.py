ecf471dda9c45c586d6ffbe15ca71d12b62a4228
Nordap64 <54553880+Nordap64@users.noreply.github.com>
2021-11-08 18:13:32 -0500

    #81 Changed command prefix back to '/'

--- bot/main.py
+++ bot/main.py
@@ -1,7 +1,7 @@
 import os
 from bot import Bot
 
-bot = Bot(prefix='$', lavalinkpass="password", lavalinkport=6969)
+bot = Bot(prefix='/', lavalinkpass="password", lavalinkport=6969)
 
 my_secret = os.environ.get('TOKEN')
 
