402f8c32ee0f3344411564378afa9d3d71afab55
ajmanzel <47645440+ajmanzel@users.noreply.github.com>
2021-11-16 16:18:54 -0500

    setting up lavalink support for heroku #69

--- bot/main.py
+++ bot/main.py
@@ -1,7 +1,20 @@
 import os
 from bot import Bot
 
-bot = Bot(prefix='/', lavalinkpass="password", lavalinkport=8080)
+ON_HEROKU = os.environ.get('ON_HEROKU')
+
+if ON_HEROKU:
+    port = int(os.environ.get('PORT', 17995))
+    heroku = "bot/applicationOpp.yml"
+    tmp_file_name = "/switching.yml"
+    local = "bot/application.yml"
+    os.rename(local, tmp_file_name)
+    os.rename(heroku, local)
+    os.rename(local, heroku)
+else:
+    port = 8080
+
+bot = Bot(prefix='/', lavalinkpass="password", lavalinkport=port)
 
 my_secret = os.environ.get('TOKEN')
 bot.connect(my_secret)
