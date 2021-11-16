d733a816d8d604979781903c5c319a63c2d0bc36
ajmanzel <47645440+ajmanzel@users.noreply.github.com>
2021-11-16 16:32:00 -0500

    trying again to get lavalink server on heroku #69

--- bot/main.py
+++ bot/main.py
@@ -5,12 +5,6 @@ ON_HEROKU = os.environ.get('ON_HEROKU')
 
 if ON_HEROKU:
     port = int(os.environ.get('PORT', 17995))
-    heroku = "bot/applicationOpp.yml"
-    tmp_file_name = "/switching.yml"
-    local = "bot/application.yml"
-    os.rename(local, tmp_file_name)
-    os.rename(heroku, local)
-    os.rename(local, heroku)
 else:
     port = 8080
 
