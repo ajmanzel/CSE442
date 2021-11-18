5abc256210d48f970f83bd23156f85cfcb07f62d
Nordap64 <54553880+Nordap64@users.noreply.github.com>
2021-11-18 16:43:35 -0500

    #81 fixed a crash bug

--- bot/main.py
+++ bot/main.py
@@ -3,13 +3,15 @@ from bot import Bot
 import fileinput
 
 port = '8080'
-port = os.environ.get('PORT')
+if os.environ.get('PORT') != None:
+    port = os.environ.get('PORT')
 textToSearch = 'DYNAMICPORT'
-fileToSearch  = "application.yml"
+fileToSearch  = "bot/application.yml"
 tempFile = open( fileToSearch, 'r+' )
 
 for line in fileinput.input(fileToSearch):
-    tempFile.write( line.replace( textToSearch, port ) )
+    if line.__contains__(textToSearch):
+        tempFile.write( line.replace( textToSearch, port ) )
 tempFile.close()
 
 bot = Bot(prefix='$', lavalinkpass="password", lavalinkport=int(port))
