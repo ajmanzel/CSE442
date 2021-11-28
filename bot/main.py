fa7f71f14e4cdc9e42b566ccc38608ae3d5f3445
ajmanzel <47645440+ajmanzel@users.noreply.github.com>
2021-11-28 14:11:38 -0500

    trying again to get lavalink server on heroku #69

--- bot/main.py
+++ bot/main.py
@@ -6,7 +6,7 @@ port = '8080'
 if os.environ.get('PORT') != None:
     port = os.environ.get('PORT')
 textToSearch = 'DYNAMICPORT'
-fileToSearch  = "bot/application.yml"
+fileToSearch = "bot/application.yml"
 tempFile = open( fileToSearch, 'r+' )
 
 for line in fileinput.input(fileToSearch):
