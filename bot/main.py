012e4fa98726a54001ebe1f1acb2ab4e2a7de364
ajmanzel <47645440+ajmanzel@users.noreply.github.com>
2021-11-28 19:15:47 -0500

    getting set up for demo tomorrow #69

--- bot/main.py
+++ bot/main.py
@@ -3,6 +3,7 @@ from bot import Bot
 import fileinput
 
 port = '8080'
+"""
 if os.environ.get('PORT') != None:
     port = os.environ.get('PORT')
 textToSearch = 'DYNAMICPORT'
@@ -13,7 +14,7 @@ for line in fileinput.input(fileToSearch):
     if line.__contains__(textToSearch):
         tempFile.write( line.replace( textToSearch, port ) )
 tempFile.close()
-
+"""
 bot = Bot(prefix='$', lavalinkpass="password", lavalinkport=int(port))
 
 my_secret = os.environ.get('TOKEN')
